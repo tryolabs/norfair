1315fc1049aefa1a13da8d002777b85c9526841f
joaqo <joaquin.alori@gmail.com>
2020-08-27 18:02:15 -0300

    Make match_distance_threshold compulsory and rename it
    It is too importante and has too much variance to be optional, the user
    has to set it. Renamed to distance_threshold.

--- norfair/tracker.py
+++ norfair/tracker.py
@@ -6,13 +6,13 @@ import random
 
 
 class Tracker:
-    def __init__(self, distance_function, hit_inertia_min=10, hit_inertia_max=25, match_distance_threshold=1,
+    def __init__(self, distance_function, distance_threshold, hit_inertia_min=10, hit_inertia_max=25, 
                  detection_threshold=0):
         self.tracked_objects = []
         self.distance_function = distance_function
         self.hit_inertia_min = hit_inertia_min
         self.hit_inertia_max = hit_inertia_max
-        self.match_distance_threshold = match_distance_threshold
+        self.distance_threshold = distance_threshold
         self.detection_threshold = detection_threshold
         TrackedObject.count = 0
 
@@ -56,7 +56,7 @@ class Tracker:
     def update_objects_in_place(self, objects, detections):
         if detections is not None and len(detections) > 0:
             distance_matrix = np.ones((len(detections), len(objects)), dtype=np.float32)
-            distance_matrix *= self.match_distance_threshold + 1
+            distance_matrix *= self.distance_threshold + 1
             for d, detection in enumerate(detections):
                 for o, obj in enumerate(objects):
                     distance = self.distance_function(detection, obj)
@@ -64,8 +64,8 @@ class Tracker:
                     # dont force the hungarian algorithm to minimize them and therefore
                     # introduce the possibility of sub optimal results.
                     # Note: This is probably not needed with the new distance minimizing algorithm
-                    if distance > self.match_distance_threshold:
-                        distance_matrix[d, o] = self.match_distance_threshold + 1
+                    if distance > self.distance_threshold:
+                        distance_matrix[d, o] = self.distance_threshold + 1
                     else:
                         distance_matrix[d, o] = distance
 
@@ -75,7 +75,7 @@ class Tracker:
             if np.isinf(distance_matrix).any():
                 print("\nReceived inf values from distance function, please check your distance function for errors!")
                 print("If you want to explicitly ignore a certain detection - tracked object pair, just")
-                print("return match_distance_threshold + 1 from your distance function.")
+                print("return distance_threshold + 1 from your distance function.")
                 exit()
 
             matched_det_indices, matched_obj_indices = self.match_dets_and_objs(distance_matrix)
@@ -87,7 +87,7 @@ class Tracker:
                     match_distance = distance_matrix[match_det_idx, match_obj_idx]
                     matched_detection = detections[match_det_idx]
                     matched_object = objects[match_obj_idx]
-                    if match_distance < self.match_distance_threshold:
+                    if match_distance < self.distance_threshold:
                         matched_object.hit(matched_detection, period=self.period)
                         matched_object.last_distance = match_distance
                     else:
@@ -106,7 +106,7 @@ class Tracker:
         cases in which this was not optimal. So now I just match by starting
         with the global minimum distance and matching the det-obj corresponding
         to that distance, then taking the second minimum, and so on until we
-        reach the match_distance_threshold.
+        reach the distance_threshold.
 
         This avoids the the algorithm getting cute with us and matching things
         that shouldn't be matching just for the sake of minimizing the global
@@ -120,14 +120,14 @@ class Tracker:
             obj_idxs = []
             current_min = distance_matrix.min()
 
-            while current_min < self.match_distance_threshold:
+            while current_min < self.distance_threshold:
                 flattened_arg_min = distance_matrix.argmin()
                 det_idx = flattened_arg_min // distance_matrix.shape[1]
                 obj_idx = flattened_arg_min % distance_matrix.shape[1]
                 det_idxs.append(det_idx)
                 obj_idxs.append(obj_idx)
-                distance_matrix[det_idx, :] = self.match_distance_threshold + 1
-                distance_matrix[:, obj_idx] = self.match_distance_threshold + 1
+                distance_matrix[det_idx, :] = self.distance_threshold + 1
+                distance_matrix[:, obj_idx] = self.distance_threshold + 1
                 current_min = distance_matrix.min()
 
             return det_idxs, obj_idxs
