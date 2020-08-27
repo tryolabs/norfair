925b5a597a4c313be0faddee85ed3a8abc4d005a
joaqo <joaquin.alori@gmail.com>
2020-08-27 18:04:13 -0300

    Add Detectron2 traffic demo

---
+++ demos/detectron2_cars.py
@@ -0,0 +1,30 @@
+import cv2
+import numpy as np
+from detectron2.engine import DefaultPredictor
+from detectron2.config import get_cfg
+import sys
+sys.path.append(".")
+from norfair import Detection, Tracker, Video, draw_tracked_objects, draw_points
+
+# Set up Detectron2 object detector
+cfg = get_cfg()
+cfg.merge_from_file("demos/faster_rcnn_R_50_FPN_3x.yaml")
+cfg.MODEL.ROI_HEADS.SCORE_THRESH_TEST = 0.5
+cfg.MODEL.WEIGHTS = "detectron2://COCO-InstanceSegmentation/mask_rcnn_R_50_FPN_3x/137849600/model_final_f10217.pkl"
+detector = DefaultPredictor(cfg)
+
+# Distance function
+def ceintroid_distance(detection, tracked_object):
+    return np.linalg.norm(detection.points - tracked_object.estimate)
+
+# Norfair
+video = Video(input_path="video.mp4")
+tracker = Tracker(distance_function=ceintroid_distance, distance_threshold=20)
+
+for frame in video:
+    detections = detector(cv2.cvtColor(frame, cv2.COLOR_BGR2RGB))
+    # Convert detections from Detectron2 format into Norfair point format
+    detections = [Detection(p) for p in detections['instances'].pred_boxes.get_centers().cpu().numpy()]
+    tracked_objects = tracker.update(detections=detections)
+    draw_tracked_objects(frame, tracked_objects)
+    video.write(frame)
