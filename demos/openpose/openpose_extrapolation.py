7ef4cf28ec551348c744c96600a59a845baaa456
joaqo <joaquin.alori@gmail.com>
2020-09-09 13:27:59 -0300

    Improve openpose demo

--- demos/openpose/openpose_extrapolation.py
+++ demos/openpose/openpose_extrapolation.py
@@ -1,3 +1,4 @@
+import argparse
 import sys
 
 import numpy as np
@@ -7,13 +8,13 @@ import norfair
 from norfair import Detection, Tracker, Video
 
 frame_skip_period = 3
-detection_threshold = 0.2
-distance_threshold = 0.3
+detection_threshold = 0.01
+distance_threshold = 0.4
 
 
 class OpenposeDetector:
     def __init__(self):
-        with open("./demos/openpose/openpose_config.yml", "r") as stream:
+        with open("openpose_config.yml", "r") as stream:
             open_pose_config = yaml.safe_load(stream)["openpose"]
         openpose_dir = open_pose_config["dir"]
         sys.path.append(openpose_dir + "/build/python/openpose")
@@ -35,33 +36,37 @@ def keypoints_distance(detected_pose, tracked_pose):
     )
     return 1 / (1 + match_num)
 
-
 pose_detector = OpenposeDetector()
-video = Video(input_path="video.mp4")
-tracker = Tracker(
-    distance_function=keypoints_distance,
-    distance_threshold=distance_threshold,
-    detection_threshold=detection_threshold,
-    point_transience=2,
-)
-keypoint_dist_threshold = video.input_height / 30
+parser = argparse.ArgumentParser(description="Track human poses in a video.")
+parser.add_argument("files", type=str, nargs="+", help="Video files to process")
+args = parser.parse_args()
 
-for i, frame in enumerate(video):
-    if i % frame_skip_period == 0:
-        detected_poses = pose_detector(frame)
-        detections = (
-            []
-            if not detected_poses.any()
-            else [
-                Detection(p, scores=s)
-                for (p, s) in zip(detected_poses[:, :, :2], detected_poses[:, :, 2])
-            ]
-        )
-        tracked_objects = tracker.update(
-            detections=detections, period=frame_skip_period
-        )
-        norfair.draw_points(frame, detections)
-    else:
-        tracked_objects = tracker.update()
-    norfair.draw_tracked_objects(frame, tracked_objects)
-    video.write(frame)
+for input_path in args.files:
+    video = Video(input_path=input_path)
+    tracker = Tracker(
+        distance_function=keypoints_distance,
+        distance_threshold=distance_threshold,
+        detection_threshold=detection_threshold,
+        point_transience=2,
+    )
+    keypoint_dist_threshold = video.input_height / 25
+
+    for i, frame in enumerate(video):
+        if i % frame_skip_period == 0:
+            detected_poses = pose_detector(frame)
+            detections = (
+                []
+                if not detected_poses.any()
+                else [
+                    Detection(p, scores=s)
+                    for (p, s) in zip(detected_poses[:, :, :2], detected_poses[:, :, 2])
+                ]
+            )
+            tracked_objects = tracker.update(
+                detections=detections, period=frame_skip_period
+            )
+            norfair.draw_points(frame, detections)
+        else:
+            tracked_objects = tracker.update()
+        norfair.draw_tracked_objects(frame, tracked_objects)
+        video.write(frame)
