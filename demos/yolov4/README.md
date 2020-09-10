31a2a6ba245f27d830f29e2beae3214fc4f0039b
Alan Descoins <dekked@gmail.com>
2020-09-10 17:58:00 -0300

    Improve YOLOv4 demo.

--- demos/yolov4/README.md
+++ demos/yolov4/README.md
@@ -5,7 +5,7 @@ Simplest possible example of tracking. Based on [pytorch YOLOv4](https://github.
 ## Instructions
 
 1. Clone [pytorch YOLOv4](https://github.com/Tianxiaomo/pytorch-YOLOv4/tree/master) and download the [weights](https://drive.google.com/open?id=1wv_LiFeCRYwtpkqREPeI13-gPELBDwuJ) published on the repo into your local clone of the repo.
-2. Copy `yolov4demo.py` into your local clone of the repo and run it.
+2. Copy `yolov4demo.py` into your local clone of the repo and run `python yolov4demo.py <video file>`.
 
 ## Explanation
 
--- demos/yolov4/yolov4demo.py
+++ demos/yolov4/yolov4demo.py
@@ -12,11 +12,14 @@ from tool.utils import load_class_names, plot_boxes_cv2
 
 max_distance_between_points = 30
 
+
 class YOLO:
     def __init__(self, weightfile, use_cuda=True):
         self.use_cuda = use_cuda
         self.model = Yolov4(yolov4conv137weight=None, n_classes=80, inference=True)
-        pretrained_dict = torch.load(weightfile, map_location=torch.device('cuda'))
+        pretrained_dict = torch.load(
+            weightfile, map_location=torch.device("cuda" if use_cuda else "cpu")
+        )
         self.model.load_state_dict(pretrained_dict)
 
         if self.use_cuda:
@@ -30,8 +33,10 @@ class YOLO:
         boxes = do_detect(self.model, sized, 0.4, 0.6, self.use_cuda)
         return boxes[0]
 
+
 def euclidean_distance(detection, tracked_object):
     return np.linalg.norm(detection.points - tracked_object.estimate)
+
 
 def get_centroid(yolo_box, img_height, img_width):
     x1 = yolo_box[0] * img_width
@@ -40,11 +45,12 @@ def get_centroid(yolo_box, img_height, img_width):
     y2 = yolo_box[3] * img_height
     return np.array([(x1 + x2) / 2, (y1 + y2) / 2])
 
+
 parser = argparse.ArgumentParser(description="Track human poses in a video.")
 parser.add_argument("files", type=str, nargs="+", help="Video files to process")
 args = parser.parse_args()
 
-model = YOLO("yolov4.pth")
+model = YOLO("yolov4.pth")  # set use_cuda=False if using CPU
 
 for input_path in args.files:
     video = Video(input_path=input_path)
@@ -57,7 +63,8 @@ for input_path in args.files:
         detections = model(frame)
         detections = [
             Detection(get_centroid(box, frame.shape[0], frame.shape[1]), data=box)
-            for box in detections if box[-1] == 2
+            for box in detections
+            if box[-1] == 2
         ]
         tracked_objects = tracker.update(detections=detections)
         norfair.draw_points(frame, detections)
