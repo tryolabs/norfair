46ceefee1acfbd834b99a061875d3b80d1f97d3b
joaqo <joaquin.alori@gmail.com>
2021-01-14 18:09:27 -0300

    Add function to cut-out rectangle from image from a set of points

--- norfair/__init__.py
+++ norfair/__init__.py
@@ -1,4 +1,4 @@
 from .drawing import *
 from .tracker import Detection, Tracker
-from .utils import print_objects_as_table
+from .utils import print_objects_as_table, get_cutout
 from .video import Video
