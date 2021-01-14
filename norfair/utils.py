2276885e2d16b77080692137636fb491c866ecb4
joaqo <joaquin.alori@gmail.com>
2021-01-14 18:09:27 -0300

    Add function to cut-out rectangle from image from a set of points

--- norfair/utils.py
+++ norfair/utils.py
@@ -59,3 +59,12 @@ def get_terminal_size(default: Tuple[int, int] = (80, 24)) -> Tuple[int, int]:
             continue
         break
     return columns, lines
+
+
+def get_cutout(points, image):
+    """Returns a rectangular cut-out from a set of points on an image"""
+    max_x = int(max(points[:, 0]))
+    min_x = int(min(points[:, 0]))
+    max_y = int(max(points[:, 1]))
+    min_y = int(min(points[:, 1]))
+    return image[min_y:max_y, min_x:max_x]
