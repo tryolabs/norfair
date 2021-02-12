eff6e8ce3399afebafb25765564c89870f0f95f8
Alan Descoins <dekked@gmail.com>
2021-02-12 15:27:37 -0300

    Fix bug when creating folder.

--- norfair/metrics.py
+++ norfair/metrics.py
@@ -229,9 +229,9 @@ class Accumulators:
         return self.summary
 
     def save_metrics(self, save_path=".", file_name="metrics.txt"):
-
         if not os.path.exists(save_path):
-            os.makedirs(save_folder)
+            os.makedirs(save_path)
+
         metrics_path = os.path.join(save_path, file_name)
         metrics_file = open(metrics_path, "w+")
         metrics_file.write(self.summary)
