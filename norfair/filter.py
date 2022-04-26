3ad99c854ceddcd34c79beede899c5fa62cca850
Agustin Castro <acastro@tryolabs.com>
2022-04-26 17:16:30 -0300

    Add 'No filter' and 'Optimized KF' setups'

--- norfair/filter.py
+++ norfair/filter.py
@@ -38,3 +38,145 @@ class FilterSetup:
         filter.P[dim_z:, dim_z:] *= self.P
 
         return filter
+
+
+class NoFilter:
+    def __init__(self, dim_x, dim_z):
+        self.dim_z = dim_z
+        self.x = np.zeros((dim_x, 1))
+
+        self.kp_undetected = np.ones((dim_z, 1))
+
+    def predict(self):
+        return
+
+    def update(self, detection_points_flatten, R=None, H=None):
+
+        if H is not None:
+            diagonal = np.diagonal(H).reshape((self.dim_z, 1))
+            one_minus_diagonal = 1 - diagonal
+            self.kp_undetected = np.logical_and(one_minus_diagonal, self.kp_undetected)
+
+            detection_points_flatten = np.multiply(
+                diagonal, detection_points_flatten
+            ) + np.multiply(one_minus_diagonal, self.x[: self.dim_z])
+
+        self.x[: self.dim_z] = detection_points_flatten
+
+
+class NoFilterSetup:
+    def create_filter(self, initial_detection: np.array):
+        num_points = initial_detection.shape[0]
+        dim_z = 2 * num_points  # flattened positions
+        dim_x = 2 * 2 * num_points  # We need to accommodate for velocities
+
+        no_filter = NoFilter(
+            dim_x, dim_z,
+        )
+        no_filter.x[:dim_z] = np.expand_dims(initial_detection.flatten(), 0).T
+        return no_filter
+
+
+class OptimizedKalmanFilter:
+    def __init__(
+        self,
+        dim_x,
+        dim_z,
+        pos_variance=10,
+        pos_vel_covariance=0,
+        vel_variance=10,
+        q=0.1,
+        r=4,
+    ):
+        self.dim_z = dim_z
+        self.x = np.zeros((dim_x, 1))
+
+        # matrix P from Kalman
+        self.pos_variance = pos_variance
+        self.pos_vel_covariance = pos_vel_covariance
+        self.vel_variance = vel_variance
+
+        self.q_Q = q
+        self.r_R = r
+
+        self.kp_undetected = np.ones((dim_z, 1))
+
+    def predict(self):
+        self.x[: self.dim_z] += self.x[self.dim_z :]
+
+    def update(self, detection_points_flatten, R=None, H=None):
+        
+        if H is not None:
+            diagonal = np.diagonal(H).reshape((self.dim_z, 1))
+
+            kp_just_detected = np.logical_and(diagonal, self.kp_undetected)
+
+            one_minus_diagonal = 1 - diagonal
+            self.kp_undetected = np.logical_and(one_minus_diagonal, self.kp_undetected)
+
+            detection_points_flatten = np.multiply(
+                diagonal, detection_points_flatten
+            ) + np.multiply(one_minus_diagonal, self.x[: self.dim_z])
+
+            kp_just_detected = np.argwhere(kp_just_detected.flatten())
+            self.x[: self.dim_z][kp_just_detected] = detection_points_flatten[kp_just_detected]
+
+        error = detection_points_flatten - self.x[: self.dim_z]
+
+        vel_var_plus_pos_vel_cov = self.pos_vel_covariance + self.vel_variance
+        added_variances = (
+            self.pos_variance
+            + self.pos_vel_covariance
+            + vel_var_plus_pos_vel_cov
+            + self.q_Q
+            + self.r_R
+        )
+
+        self.x[: self.dim_z] = (
+            detection_points_flatten - (self.r_R / added_variances) * error
+        )
+        self.x[self.dim_z :] += (vel_var_plus_pos_vel_cov / added_variances) * error
+
+        self.pos_variance = self.r_R * (1 - self.r_R / added_variances)
+        self.pos_vel_covariance = (
+            (vel_var_plus_pos_vel_cov) * self.r_R / added_variances
+        )
+        self.vel_variance += (
+            self.q_Q - (vel_var_plus_pos_vel_cov ** 2) / added_variances
+        )
+
+
+class OptimizedKalmanFilterSetup:
+    def __init__(
+        self,
+        R: float = 4.0,
+        Q: float = 0.1,
+        pos_variance: float = 1,
+        pos_vel_covariance: float = 0,
+        vel_variance: float = 10,
+    ):
+        self.R = R
+        self.Q = Q
+
+        # entrances P matrix of KF
+        self.pos_variance = pos_variance
+        self.pos_vel_covariance = pos_vel_covariance
+        self.vel_variance = vel_variance
+
+    def create_filter(self, initial_detection: np.array):
+        num_points = initial_detection.shape[0]
+        dim_z = 2 * num_points  # flattened positions
+        dim_x = 2 * 2 * num_points  # We need to accommodate for velocities
+
+        custom_filter = OptimizedKalmanFilter(
+            dim_x,
+            dim_z,
+            pos_variance=self.pos_variance,
+            pos_vel_covariance=self.pos_vel_covariance,
+            vel_variance=self.vel_variance,
+            q=self.Q,
+            r=self.R,
+        )
+        custom_filter.x[:dim_z] = np.expand_dims(initial_detection.flatten(), 0).T
+
+        return custom_filter
