fb9a5340fc27d1a987c43cc1f895bad9046e0cbe
Agustin Castro <acastro@tryolabs.com>
2022-04-27 11:17:38 -0300

    Set FilterSetup as the default setup for motmetrics4norfair

--- demos/motmetrics4norfair/motmetrics4norfair.py
+++ demos/motmetrics4norfair/motmetrics4norfair.py
@@ -3,7 +3,7 @@ import os.path
 
 import numpy as np
 
-from norfair import Tracker, drawing, metrics, video
+from norfair import Tracker, drawing, metrics, video, FilterSetup
 
 frame_skip_period = 1
 detection_threshold = 0.01
@@ -124,6 +124,7 @@ for input_path in sequences_paths:
         detection_threshold=detection_threshold,
         pointwise_hit_counter_max=pointwise_hit_counter_max,
         hit_counter_max=hit_counter_max,
+        filter_setup=FilterSetup(),
     )
 
     # Initialize accumulator for this video
