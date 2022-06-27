d3d0f13be33472f08eb859c1fd045b476b0f56f7
fatih <34196005+fcakyon@users.noreply.github.com>
2022-06-27 21:25:04 +0300

    Add version to project's __init__.py (#123)
    Usage:
    
    ```python
    import norfair
    
    norfair.__version__
    ```

--- norfair/__init__.py
+++ norfair/__init__.py
@@ -3,3 +3,5 @@ from .tracker import Detection, Tracker
 from .filter import FilterPyKalmanFilterFactory, OptimizedKalmanFilterFactory, NoFilterFactory
 from .utils import get_cutout, print_objects_as_table
 from .video import Video
+
+__version__ = '1.0.0'
