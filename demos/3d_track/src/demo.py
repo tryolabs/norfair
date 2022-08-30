1e39eef8a28e64b260b7471aa9f37631e975dc56
Agustin Castro <aguscas@ada.tryolabs.com>
2022-08-30 16:43:04 -0300

    Draw paths

--- demos/3d_track/src/demo.py
+++ demos/3d_track/src/demo.py
@@ -5,6 +5,7 @@ import cv2
 import mediapipe as mp
 
 from norfair import Video, Detection, Tracker
+from norfair.drawing import Paths
 from norfair.distances import mean_euclidean
 
 from utils import PixelCoordinatesProjecter, draw_3d_tracked_boxes
@@ -13,9 +14,10 @@ parser = argparse.ArgumentParser(description="3d-Track objects in a video.")
 parser.add_argument("files", type=str, nargs="+", help="Video files to process")
 parser.add_argument("--model-name", type=str, default="Shoe", help="model name ('Shoe', 'Chair', 'Cup', 'Camera')")
 parser.add_argument("--max-objects", type=int, default="2", help="Maximum number of objects at a time")
-parser.add_argument("--conf-threshold", type=float, default="0.1", help="Detector threshold") # 0.2
-parser.add_argument("--distance-threshold", type=float, default="0.5", help="Detector threshold") # 0.2
+parser.add_argument("--conf-threshold", type=float, default="0.1", help="Detector threshold") 
+parser.add_argument("--distance-threshold", type=float, default="0.5", help="Detector threshold")
 parser.add_argument("--output-path", type=str, default=".", help="Output path")
+parser.add_argument("--draw-paths", action="store_true", help="Draw path of the centroid")
 args = parser.parse_args()
 
 mp_objectron = mp.solutions.objectron
@@ -38,6 +40,12 @@ with mp_objectron.Objectron(static_image_mode=True,
                 # Set function that projects 3d objets to the pixel space
                 projecter = PixelCoordinatesProjecter(frame.shape[:2][::-1])
 
+                if args.draw_paths:
+                    # initialize path drawer
+                    def get_points_to_draw(points3d):
+                        return [projecter.eye_2_pixel(points3d)[0].astype(int)]
+                    path_drawer = Paths(get_points_to_draw)
+
             detections = []
 
             # Convert the BGR image to RGB and process it with MediaPipe Objectron.
@@ -56,4 +64,7 @@ with mp_objectron.Objectron(static_image_mode=True,
             tracked_objects = tracker.update(detections)
 
             frame = draw_3d_tracked_boxes(frame, tracked_objects, projecter=projecter.eye_2_pixel)
+            if args.draw_paths:
+                frame = path_drawer.draw(frame, tracked_objects)
+
             video.write(frame)
