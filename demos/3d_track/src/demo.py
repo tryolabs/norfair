c50eafb3da32de042f29fe8fb0cded9bb2e2b6dc
Agustin Castro <aguscas@ada.tryolabs.com>
2022-08-30 15:34:24 -0300

    Create class that projects 3d objects to pixel space

--- demos/3d_track/src/demo.py
+++ demos/3d_track/src/demo.py
@@ -4,8 +4,10 @@ import numpy as np
 import cv2
 import mediapipe as mp
 
-from norfair import Video, Detection, Tracker, Color
+from norfair import Video, Detection, Tracker
 from norfair.distances import mean_euclidean
+
+from utils import PixelCoordinatesProjecter, draw_3d_tracked_boxes
 
 parser = argparse.ArgumentParser(description="3d-Track objects in a video.")
 parser.add_argument("files", type=str, nargs="+", help="Video files to process")
@@ -16,7 +18,6 @@ parser.add_argument("--distance-threshold", type=float, default="0.5", help="Det
 parser.add_argument("--output-path", type=str, default=".", help="Output path")
 args = parser.parse_args()
 
-mp_drawing = mp.solutions.drawing_utils
 mp_objectron = mp.solutions.objectron
 
 with mp_objectron.Objectron(static_image_mode=True,
@@ -31,31 +32,28 @@ with mp_objectron.Objectron(static_image_mode=True,
             distance_function=mean_euclidean,
             distance_threshold=args.distance_threshold,
         )
-        for image in video:
+        projecter = None
+        for frame in video:
+            if projecter is None:
+                # Set function that projects 3d objets to the pixel space
+                projecter = PixelCoordinatesProjecter(frame.shape[:2][::-1])
+
             detections = []
 
             # Convert the BGR image to RGB and process it with MediaPipe Objectron.
-            results = objectron.process(cv2.cvtColor(image, cv2.COLOR_BGR2RGB))
+            results = objectron.process(cv2.cvtColor(frame, cv2.COLOR_BGR2RGB))
             
-            # Draw box landmarks.
             if not results.detected_objects:
-                video.write(image)
+                video.write(frame)
                 tracker.update(detections)
                 continue
  
-            annotated_image = image.copy()
             for detected_object in results.detected_objects:
- 
                 points = np.array([[p.x, p.y, p.z] for p in detected_object.landmarks_3d.landmark])
-                detections.append(Detection(points=points, data=detected_object))
+                detections.append(Detection(points=points))
 
 
             tracked_objects = tracker.update(detections)
-            
-            for obj in tracked_objects:
-                color_spec = mp_drawing.DrawingSpec(color=Color.random(obj.id), thickness=5)
-                mp_drawing.draw_landmarks(annotated_image, obj.last_detection.data.landmarks_2d, mp_objectron.BOX_CONNECTIONS, color_spec, color_spec)
-                # mp_drawing.draw_axis(annotated_image, obj.last_detection.data.rotation, obj.last_detection.data.translation)
 
-            video.write(annotated_image)
-            
+            frame = draw_3d_tracked_boxes(frame, tracked_objects, projecter=projecter.eye_2_pixel)
+            video.write(frame)
