fc95e109e9f3c164dc08a62bda4c63512989cf85
Alan Descoins <dekked@gmail.com>
2022-08-31 13:57:28 -0300

    Add missing flags to Docker.

--- demos/yolov5/run_gpu.sh
+++ demos/yolov5/run_gpu.sh
@@ -2,6 +2,7 @@
 docker build . -t norfair-yolov5
 docker run -it --rm \
            --gpus all \
+           --shm-size=1gb \
            -v `realpath .`:/demo \
            norfair-yolov5 \
            bash
