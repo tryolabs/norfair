415aee84d89f1391cfe4e1eae1775c75396ea9cc
Agustin Castro <acastro@tryolabs.com>
2022-09-01 09:41:49 -0300

    Fix that objects were not drawn in frames without detections

--- demos/3d_track/src/demo.py
+++ demos/3d_track/src/demo.py
@@ -10,7 +10,7 @@ from norfair.drawing import Paths
 from utils import PixelCoordinatesProjecter, draw_3d_tracked_boxes, scaled_euclidean
 
 
-parser = argparse.ArgumentParser(description="3d-Track objects in a video.")
+parser = argparse.ArgumentParser(description="3d-Track objects in a video")
 parser.add_argument("files", type=str, nargs="+", help="Video files to process")
 parser.add_argument("--model-name", type=str, default="Shoe", help="model name ('Shoe', 'Chair', 'Cup', 'Camera')")
 parser.add_argument("--max-objects", type=int, default="2", help="Maximum number of objects at a time")
@@ -20,6 +20,7 @@ parser.add_argument("--conf-threshold", type=float, default="0.1", help="Detecto
 parser.add_argument("--distance-threshold", type=float, default="0.5", help="Distance threshold")
 parser.add_argument("--output-path", type=str, default=".", help="Output path")
 parser.add_argument("--draw-paths", action="store_true", help="Draw path of the centroid")
+parser.add_argument("--draw-only-alive", action="store_true", help="Draw only the recently matched trackers")
 args = parser.parse_args()
 
 mp_objectron = mp.solutions.objectron
@@ -50,23 +51,20 @@ with mp_objectron.Objectron(static_image_mode=True,
                         return [projecter.eye_2_pixel(points3d)[0].astype(int)]
                     path_drawer = Paths(get_points_to_draw)
 
-            detections = []
-
             # Convert the BGR image to RGB and process it with MediaPipe Objectron.
             results = objectron.process(cv2.cvtColor(frame, cv2.COLOR_BGR2RGB))
             
-            if not results.detected_objects:
-                video.write(frame)
-                tracker.update(detections)
-                continue
- 
-            for detected_object in results.detected_objects:
-                points = np.array([[p.x, p.y, p.z] for p in detected_object.landmarks_3d.landmark])
-                detections.append(Detection(points=points))
-
+            detections = []
+            try:
+                for detected_object in results.detected_objects:
+                    points = np.array([[p.x, p.y, p.z] for p in detected_object.landmarks_3d.landmark])
+                    detections.append(Detection(points=points))
+            except TypeError:
+                pass
+            
             tracked_objects = tracker.update(detections)
 
-            frame = draw_3d_tracked_boxes(frame, tracked_objects, projecter=projecter.eye_2_pixel)
+            frame = draw_3d_tracked_boxes(frame, tracked_objects, projecter=projecter.eye_2_pixel, draw_only_alive=args.draw_only_alive)
             if args.draw_paths:
                 frame = path_drawer.draw(frame, tracked_objects)
 
