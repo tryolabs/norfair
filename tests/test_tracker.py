58221f340d052820ae830a983a46ebca0589fc26
javiber <javier.berneche@gmail.com>
2022-09-01 14:27:47 -0300

    Changed behavior of Tracker
    - fixed problem where hit_counter could get bigger than the specified max
    - adpated the check between delay and max in tracker's init
    - added check to prevent objects with negative count to be returned
    - updated test_tracker accordingly

--- tests/test_tracker.py
+++ tests/test_tracker.py
@@ -11,67 +11,109 @@ from norfair import (
 )
 
 
-@pytest.mark.parametrize("filter_factory", [FilterPyKalmanFilterFactory(), OptimizedKalmanFilterFactory()])
+def test_params():
+    #
+    # test some invalid initializations
+    #
+    with pytest.raises(ValueError):
+        Tracker("frobenius", distance_threshold=10, initialization_delay=-1)
+    with pytest.raises(ValueError):
+        Tracker(
+            "frobenius",
+            distance_threshold=10,
+            initialization_delay=1,
+            hit_counter_max=0,
+        )
+    with pytest.raises(ValueError):
+        Tracker(
+            "frobenius",
+            distance_threshold=10,
+            initialization_delay=1,
+            hit_counter_max=1,
+        )
+    with pytest.raises(ValueError):
+        Tracker(
+            "_bad_distance",
+            distance_threshold=10,
+            initialization_delay=1,
+            hit_counter_max=1,
+        )
+
+
+@pytest.mark.parametrize(
+    "filter_factory", [FilterPyKalmanFilterFactory(), OptimizedKalmanFilterFactory()]
+)
 def test_simple(filter_factory):
-    #
-    # tests a simple static detection
-    #
-    tracker = Tracker("frobenius", initialization_delay=3, distance_threshold=100, hit_counter_max=3, filter_factory=filter_factory)
+    for delay in [0, 1, 3]:
+        for counter_max in [delay + 1, delay + 3]:
+            #
+            # tests a simple static detection
+            #
+            tracker = Tracker(
+                "frobenius",
+                initialization_delay=delay,
+                distance_threshold=100,
+                hit_counter_max=counter_max,
+                filter_factory=filter_factory,
+            )
 
-    # simulate 4 consecutive frames receiving the same detection
-    detections = [Detection(points=np.array([[1, 1]]))]
-    assert len(tracker.update(detections)) == 0
-    assert len(tracker.update(detections)) == 0
-    assert len(tracker.update(detections)) == 0
-    tracked_objects = tracker.update(detections)
-    assert len(tracked_objects) == 1
-    obj = tracked_objects[0]
-    np.testing.assert_almost_equal(tracked_objects[0].estimate, np.array([[1, 1]]))
-    assert obj.age == 3
-    assert obj.hit_counter == 4 # TODO: check this result
+            detections = [Detection(points=np.array([[1, 1]]))]
 
-    # simulate that the object dissapears
-    tracked_objects = tracker.update()
-    assert len(tracked_objects) == 1
-    obj = tracked_objects[0]
-    assert obj.age == 4
-    assert obj.hit_counter == 3
+            # test the delay
+            for age in range(delay):
+                assert len(tracker.update(detections)) == 0
 
-    tracked_objects = tracker.update()
-    assert len(tracked_objects) == 1
-    obj = tracked_objects[0]
-    assert obj.age == 5
-    assert obj.hit_counter == 2
+            # build up hit_counter from delay+1 to counter_max
+            for age in range(delay, counter_max):
+                tracked_objects = tracker.update(detections)
+                assert len(tracked_objects) == 1
+                obj = tracked_objects[0]
+                np.testing.assert_almost_equal(
+                    tracked_objects[0].estimate, np.array([[1, 1]])
+                )
+                assert obj.age == age
+                assert obj.hit_counter == age + 1
 
+            # check that counter is capped at counter_max
+            for age in range(counter_max, counter_max + 3):
+                tracked_objects = tracker.update(detections)
+                assert len(tracked_objects) == 1
+                obj = tracked_objects[0]
+                np.testing.assert_almost_equal(
+                    tracked_objects[0].estimate, np.array([[1, 1]])
+                )
+                assert obj.age == age
+                assert obj.hit_counter == counter_max
 
-    tracked_objects = tracker.update()
-    assert len(tracked_objects) == 1
-    obj = tracked_objects[0]
-    assert obj.age == 6
-    assert obj.hit_counter == 1
+            # check that counter goes down to 0 wen no detections
+            for counter in range(counter_max - 1, -1, -1):
+                age += 1
+                tracked_objects = tracker.update()
+                assert len(tracked_objects) == 1
+                obj = tracked_objects[0]
+                np.testing.assert_almost_equal(
+                    tracked_objects[0].estimate, np.array([[1, 1]])
+                )
+                assert obj.age == age
+                assert obj.hit_counter == counter
 
-
-    tracked_objects = tracker.update()
-    assert len(tracked_objects) == 1
-    obj = tracked_objects[0]
-    assert obj.age == 7
-    assert obj.hit_counter == 0
-
-    tracked_objects = tracker.update()
-    assert len(tracked_objects) == 1
-    obj = tracked_objects[0]
-    assert obj.age == 8
-    assert obj.hit_counter == -1 # TODO: check this result
-
-    assert len(tracker.update()) == 0
+            # check that object dissapears in the next frame
+            assert len(tracker.update()) == 0
 
 
-@pytest.mark.parametrize("filter_factory", [FilterPyKalmanFilterFactory(), OptimizedKalmanFilterFactory()])
+@pytest.mark.parametrize(
+    "filter_factory", [FilterPyKalmanFilterFactory(), OptimizedKalmanFilterFactory()]
+)
 def test_moving(filter_factory):
     #
     # Test a simple case of a moving object
     #
-    tracker = Tracker("frobenius", initialization_delay=3, distance_threshold=100, filter_factory=filter_factory)
+    tracker = Tracker(
+        "frobenius",
+        initialization_delay=3,
+        distance_threshold=100,
+        filter_factory=filter_factory,
+    )
 
     assert len(tracker.update([Detection(points=np.array([[1, 1]]))])) == 0
     assert len(tracker.update([Detection(points=np.array([[1, 2]]))])) == 0
@@ -84,13 +126,19 @@ def test_moving(filter_factory):
     assert 3 < tracked_objects[0].estimate[0][1] <= 4
 
 
-
-@pytest.mark.parametrize("filter_factory", [FilterPyKalmanFilterFactory(), OptimizedKalmanFilterFactory()])
+@pytest.mark.parametrize(
+    "filter_factory", [FilterPyKalmanFilterFactory(), OptimizedKalmanFilterFactory()]
+)
 def test_distance_t(filter_factory):
     #
     # Test a moving object with a small distance threshold
     #
-    tracker = Tracker("frobenius", initialization_delay=1, distance_threshold=1, filter_factory=filter_factory)
+    tracker = Tracker(
+        "frobenius",
+        initialization_delay=1,
+        distance_threshold=1,
+        filter_factory=filter_factory,
+    )
 
     # should not match because the distance is too large
     assert len(tracker.update([Detection(points=np.array([[1, 1]]))])) == 0
