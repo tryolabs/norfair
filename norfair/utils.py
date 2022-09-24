e519e0ae155976559e25d6f92976f829d477005a
Alan Descoins <dekked@gmail.com>
2022-09-24 19:15:36 -0300

    Fix broken link to documentation in error message.

--- norfair/utils.py
+++ norfair/utils.py
@@ -24,7 +24,9 @@ def print_detection_error_message_and_exit(points):
         f"Each `Detection` object should have a property `points` of shape (num_of_points_to_track, 2), not {points.shape}. Check your `Detection` list creation code."
     )
     print("You can read the documentation for the `Detection` class here:")
-    print("https://github.com/tryolabs/norfair/tree/master/docs#detection\n")
+    print(
+        "https://tryolabs.github.io/norfair/reference/tracker/#norfair.tracker.Detection\n"
+    )
     exit()
 
 
