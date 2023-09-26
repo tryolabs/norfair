42af0a27be68884306645b3a55724a145f3e8d6f
Boris Teodorovich <boris@degirum.com>
2023-09-25 18:53:59 -0700

    Added options to stream from camera and show output

--- demos/degirum/src/demo.py
+++ demos/degirum/src/demo.py
@@ -9,28 +9,34 @@ from norfair import Detection, Paths, Tracker, Video
 
 parser = argparse.ArgumentParser(description="Track objects in a video.")
 parser.add_argument(
-    "files", type=str, nargs="+", help="Video files to process"
+    "input", type=str, nargs='?', help="Video file to process"
+)
+parser.add_argument(
+    "--camera", type=int, help="Camera ID"
+)
+parser.add_argument(
+    "--show", action='store_true', help="Display in real-time"
 )
 parser.add_argument(
     "--zoo-url", type=str, required=True, help="Model URL for DeGirum cloud platform"
 )
 parser.add_argument(
-    "--model-name", type=str, default="yolo_v5s_coco--512x512_quant_n2x_cpu_1", help="Model name"
+    "--token", type=str, required=True, help="Token for DeGirum cloud platform"
 )
 parser.add_argument(
     "--inference-option", type=str, default='local', help="Inference location: 'local' 'server' or 'cloud'"
 )
 parser.add_argument(
-    "--token", type=str, required=True, help="Token for DeGirum cloud platform"
+    "--model-name", type=str, default="yolo_v5s_coco--512x512_quant_n2x_cpu_1", help="Model name"
 )
 parser.add_argument(
-    "--conf-threshold", type=float, default="0.25", help="Object confidence threshold",
+    "--conf-threshold", type=float, default="0.25", help="Object confidence threshold"
 )
 parser.add_argument(
     "--iou-threshold", type=float, default="0.45", help="IOU threshold for NMS"
 )
 parser.add_argument(
-    "--classes", nargs="+", type=str, help="Filter by class label(s): --classes car [person bicycle ...]",
+    "--classes", nargs="+", type=str, help="Filter by class label(s): --classes car [person bicycle ...]"
 )
 args = parser.parse_args()
 
@@ -40,24 +46,35 @@ model = zoo.load_model(args.model_name)
 model.output_conf_threshold = args.conf_threshold
 model.output_nms_threshold = args.iou_threshold
 
-for input_path in args.files:
-    video = Video(input_path=input_path)
+if args.camera is None and args.input is None:
+  raise Exception("Either camera id or input path is required")
+elif args.camera is not None and args.input is not None:
+  raise Exception("Only one of camera id or input path must be specified")
 
-    tracker = Tracker(
-        distance_function="iou",
-        distance_threshold=0.7
-    )
+if args.camera is not None:
+  video = Video(camera=args.camera)
+else:
+  video = Video(input_path=args.input)
 
-    for model_output in model.predict_batch(video):
-        detections = [
-          Detection(
-            points=np.array(detection["bbox"]).reshape((2, 2)),
-            scores=np.array([detection["score"]] * 2),
-            label=detection["label"]
-          ) for detection in model_output.results if (args.classes is None or detection["label"] in args.classes)
-        ]
-        tracked_objects = tracker.update(detections=detections)
-        frame = model_output.image.copy()  # Results contain corresponding input frame. Make copy for overlay
-        norfair.draw_boxes(frame, detections)
-        norfair.draw_points(frame, tracked_objects)
+tracker = Tracker(
+    distance_function="iou",
+    distance_threshold=0.7
+)
+
+for model_output in model.predict_batch(video):
+    detections = [
+      Detection(
+        points=np.array(detection["bbox"]).reshape((2, 2)),
+        scores=np.array([detection["score"]] * 2),
+        label=detection["label"]
+      ) for detection in model_output.results if (args.classes is None or detection["label"] in args.classes)
+    ]
+    tracked_objects = tracker.update(detections=detections)
+    frame = model_output.image.copy()  # Results contain corresponding input frame. Make copy for overlay
+    norfair.draw_boxes(frame, detections)
+    norfair.draw_points(frame, tracked_objects)
+    if args.input is not None:
         video.write(frame)
+    if args.camera is not None or args.show:
+        if video.show(frame) == ord('q'):
+            break
