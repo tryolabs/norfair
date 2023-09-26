a9f9c56416356fef159a40e032206f1873ccb9d4
Boris Teodorovich <boris@degirum.com>
2023-09-25 19:18:41 -0700

    Fixed pylint again using .pylintrc

--- demos/degirum/src/demo.py
+++ demos/degirum/src/demo.py
@@ -20,7 +20,7 @@ parser.add_argument(
     "--show", action='store_true', help="Display in real-time"
 )
 parser.add_argument(
-    "--zoo-url", type=str, required=True, help="Model Zoo URL for DeGirum Cloud Platform"
+    "--zoo-url", type=str, required=True, help="DeGirum Cloud Platform Model Zoo URL"
 )
 parser.add_argument(
     "--token", type=str, required=True, help="Token for DeGirum cloud platform"
@@ -29,7 +29,7 @@ parser.add_argument(
     "--device", type=str, default='cloud', help="'local' 'cloud' or device hostname"
 )
 parser.add_argument(
-    "--model-name", type=str, default="yolo_v5s_coco--512x512_quant_n2x_orca_1", help="Model name"
+    "--model", type=str, default="yolo_v5s_coco--512x512_quant_n2x_orca_1", help="Model"
 )
 parser.add_argument(
     "--conf-threshold", type=float, default="0.25", help="Object confidence threshold"
@@ -38,13 +38,13 @@ parser.add_argument(
     "--iou-threshold", type=float, default="0.45", help="IOU threshold for NMS"
 )
 parser.add_argument(
-    "--classes", nargs="+", type=str, help="Filter by class label(s): --classes car [person ...]"
+    "--classes", nargs="+", type=str, help="Filter by class label(s)"
 )
 args = parser.parse_args()
 
 device = {"local": dg.LOCAL, "cloud": dg.CLOUD}.get(args.device, args.device)
 zoo = dg.connect(device, args.zoo_url, args.token)
-model = zoo.load_model(args.model_name)
+model = zoo.load_model(args.model)
 model.output_conf_threshold = args.conf_threshold
 model.output_nms_threshold = args.iou_threshold
 
@@ -63,16 +63,16 @@ tracker = Tracker(
     distance_threshold=0.7
 )
 
-for model_output in model.predict_batch(video):
+for output in model.predict_batch(video):
     detections = [
       Detection(
         points=np.array(det["bbox"]).reshape((2, 2)),
         scores=np.array([det["score"]] * 2),
         label=det["label"]
-      ) for det in model_output.results if (args.classes is None or det["label"] in args.classes)
+      ) for det in output.results if (not args.classes or det["label"] in args.classes)
     ]
     tracked_objects = tracker.update(detections=detections)
-    frame = model_output.image.copy()  # Results contain corresponding input frame
+    frame = output.image.copy()  # Results contain corresponding input frame
     norfair.draw_boxes(frame, detections)
     norfair.draw_points(frame, tracked_objects)
     if args.input is not None:
